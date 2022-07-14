98f4a03132dc842aafef92fb233710a676e191e7
geniuus <cross_man@naver.com>
2022-07-14 16:51:07 +0900

    My First Commit & First iOS Project

---
+++ HelloiOS/HelloiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloiOS
+//
+//  Created by 유영재 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
