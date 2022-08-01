b578e6a7dcf01529ed426fa14067a15acad769c4
geniuus <cross_man@naver.com>
2022-08-01 15:39:29 +0900

    Combine을 이용한 개선버전 App,,

---
+++ Part4_Combine/Improve Combine/AppleFramework_Combine/AppleFramework/FrameworkDetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FrameworkDetailViewController.swift
+//  AppleFramework
+//
+//  Created by 유영재 2022/07/30.
+//
+
+import UIKit
+import SafariServices
+import Combine
+
+class FrameworkDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // Combine
+    var subscriptions = Set<AnyCancellable>()
+    let buttonTapped = PassthroughSubject<AppleFramework,Never>()
+    let framework = CurrentValueSubject<AppleFramework,Never>(AppleFramework(name: "Unknown", imageName: "", urlString: "", description: ""))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
+    private func bind() {
+        // input : button 클릭
+        buttonTapped
+            .receive(on: RunLoop.main)
+            .compactMap { URL(string: $0.urlString) }
+            .sink { [unowned self] url in
+                let safari = SFSafariViewController(url: url)
+                self.present(safari, animated: true)
+            }.store(in: &subscriptions)
+        
+        // output : data세팅될 때 UI update
+        framework
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] framework in
+                self.imageView.image = UIImage(named: framework.imageName)
+                self.titleLabel.text = framework.name
+                self.descriptionLabel.text = framework.description
+            }.store(in: &subscriptions)
+    }
+    
+    @IBAction func learnMoreTapped(_ sender: Any) {
+        buttonTapped.send(framework.value)
+    }
+}
